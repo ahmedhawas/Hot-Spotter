a2ccf99b4e2b5a6e19d2eb4c97718ae8c8c2caa6
ahmedhawas <ahmedhawas7@gmail.com>
2013-10-07 17:10:32 -0400

    adding user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   # attr_accessible :title, :body
+  attr_accessible :username, :email, :password
+
+  validates_presence_of :username, :on => :create
+  validates_presence_of :email, :on => :create
+  validates_presence_of :password, :on => :create
 end
