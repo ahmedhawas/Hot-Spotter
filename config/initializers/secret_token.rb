62fb9bbf0c68c880bdfdaff0ef3c1233174cf0ec
ahmedhawas <ahmedhawas7@gmail.com>
2013-10-07 15:45:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotspotter::Application.config.secret_token = 'bb4a5b45171a2021398e67e32911cabd89c3753dc04d22e0d5d59c68ac22fe73446136dd1ecfe0c02ab7fe7890c123f053b6ce019496814e2fe013a6e9e8bc8f'
