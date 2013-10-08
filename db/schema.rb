a59c50ae603083a33e11360d63639c3f14e26ef2
ahmedhawas <ahmedhawas7@gmail.com>
2013-10-08 12:15:57 -0400

    merging files

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131008005508) do
-
+ActiveRecord::Schema.define(:version => 20131008020027) do
 
   create_table "updates", :force => true do |t|
     t.string   "comment"
