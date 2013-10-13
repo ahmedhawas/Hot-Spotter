d6b8a65a36dd62a081234339a1872fe1dfc414ae
amber-h <amber.houle3@gmail.com>
2013-10-13 17:51:41 -0600

    adding background and fixing fonts

--- app/assets/javascripts/mapStyle.js
+++ app/assets/javascripts/mapStyle.js
@@ -1,48 +1,61 @@
-styles = [
-	{
-		featureType: 'water',
-		elementType: 'all',
-		stylers: [
-			{ hue: '#0099FF' },
-			{ saturation: 100 },
-			{ lightness: -34 },
-			{ visibility: 'on' }
-		]
-	},{
-		featureType: 'landscape',
-		elementType: 'all',
-		stylers: [
-			{ hue: '#00CC66' },
-			{ saturation: 100 },
-			{ lightness: -55 },
-			{ visibility: 'on' }
-		]
-	},{
-		// featureType: 'road.highway',
-		// elementType: 'all',
-		// stylers: [
-		// 	{ hue: '#888888' },
-		// 	{ saturation: -100 },
-		// 	{ lightness: -17 },
-		// 	{ visibility: 'on' }
-		// ]
-	},{
-		featureType: 'landscape',
-		elementType: 'all',
-		stylers: [
-			{ hue: '#E8E8E8' },
-			{ saturation: -100 },
-			{ lightness: 18 },
-			{ visibility: 'on' }
-		]
-	},{
-		featureType: 'poi.park',
-		elementType: 'all',
-		stylers: [
-			{ hue: '#00CC66' },
-			{ saturation: 100 },
-			{ lightness: -49 },
-			{ visibility: 'on' }
+// styles = [
+// 	{
+// 		featureType: 'water',
+// 		elementType: 'all',
+// 		stylers: [
+// 			{ hue: '#0099FF' },
+// 			{ saturation: 100 },
+// 			{ lightness: -34 },
+// 			{ visibility: 'on' }
+// 		]
+// 	},{
+// 		featureType: 'landscape',
+// 		elementType: 'all',
+// 		stylers: [
+// 			{ hue: '#00CC66' },
+// 			{ saturation: 100 },
+// 			{ lightness: -55 },
+// 			{ visibility: 'on' }
+// 		]
+// 	},{
+// 		// featureType: 'road.highway',
+// 		// elementType: 'all',
+// 		// stylers: [
+// 		// 	{ hue: '#888888' },
+// 		// 	{ saturation: -100 },
+// 		// 	{ lightness: -17 },
+// 		// 	{ visibility: 'on' }
+// 		// ]
+// 	},{
+// 		featureType: 'landscape',
+// 		elementType: 'all',
+// 		stylers: [
+// 			{ hue: '#E8E8E8' },
+// 			{ saturation: -100 },
+// 			{ lightness: 18 },
+// 			{ visibility: 'on' }
+// 		]
+// 	},{
+// 		featureType: 'poi.park',
+// 		elementType: 'all',
+// 		stylers: [
+// 			{ hue: '#00CC66' },
+// 			{ saturation: 100 },
+// 			{ lightness: -49 },
+// 			{ visibility: 'on' }
+// 		]
+// 	}
+// ];
+
+styles = [{
+	featureType: 'all',
+	elementType: 'all',
+	stylers: [
+			{ hue: '#00a1ff' },
+			{ saturation: -72 },
+			{ gamma: 0.23 },
 		]
 	}
 ];
+
+
