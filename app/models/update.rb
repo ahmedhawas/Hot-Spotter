25ebfdbea5c253914762ced5977469bdf74b8a2e
ahmedhawas <ahmedhawas7@gmail.com>
2013-10-13 17:26:23 -0400

    adding firebase adding of updates from updates_controller

--- app/models/update.rb
+++ app/models/update.rb
@@ -3,7 +3,6 @@ class Update < ActiveRecord::Base
   acts_as_likeable
 
   validates_presence_of :comment, :on => :create
-  Firebase.base_uri = 'https://hot-spotter.firebaseio.com/'
 
   
   has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "150x150>" }
