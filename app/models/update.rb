5b89eaf09849a3f67d9355d99bcd7ef3d591a5ec
amber-h <amber.houle3@gmail.com>
2013-10-13 10:44:17 -0600

    adding firebase url to update.rb

--- app/models/update.rb
+++ app/models/update.rb
@@ -3,6 +3,8 @@ class Update < ActiveRecord::Base
   acts_as_likeable
 
   validates_presence_of :comment, :on => :create
+  Firebase.base_uri = 'https://hot-spotter.firebaseio.com/'
 
+  
   has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "150x150>" }
 end
