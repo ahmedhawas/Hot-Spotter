ddbb53dc918b6ea0ac0260827aeeff727a4bdb0d
amber-h <amber.houle3@gmail.com>
2013-10-16 16:42:20 -0400

    toggling / styling map markers and adding map category buttons

--- app/assets/javascripts/updateMarker.js
+++ app/assets/javascripts/updateMarker.js
@@ -1,27 +1,30 @@
-$(function(){
+// $(function(){
+	function addUpdateMarker(){
 	var toggle = 0;
 
 	$('.updateContainer').click( function() {
 		var lat = $(this).attr('data-lat');
 	 	var lng = $(this).attr('data-lng');
+	 	var markerImg = $(this).attr('data-img');
 	 	
  		var pos = new google.maps.LatLng(lat, lng);
 
  		if (toggle == 0){
+ 			console.log(markerImg);
 		 	updateMarker = new google.maps.Marker({
 	        position: pos,
 	        map: map,
-	        title: "Update Location"
+	        title: "Update Location",
+	        icon: markerImg
 	      	});
 	      	toggle = 1;
-	      	updateMarker.set("id", "ABC");
+	      	map.setCenter(pos);
+	      	map.setZoom(16);
 	 	}
 	 	else{
-	 		console.log('does it enter the setmap null?');
-	 		console.log('marker id' + updateMarker.id);
 	 		updateMarker.setMap(null);
 	 		toggle = 0;
 	 	}
 	});
-
-)};
+	};
+// )};
