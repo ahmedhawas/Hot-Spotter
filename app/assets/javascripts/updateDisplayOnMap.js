11f6bed89f15a026d7bf6b59bfa46ea872c3f583
ahmedhawas <ahmedhawas7@gmail.com>
2013-10-25 13:43:28 -0400

    fixing title to Sizzle from Hotspotter

--- app/assets/javascripts/updateDisplayOnMap.js
+++ app/assets/javascripts/updateDisplayOnMap.js
@@ -1,17 +1,17 @@
-$(function(){
-   $('.updateContainer').click( function() {
-    var marker;
-    var marker_location;
-    var lat = parseFloat($(this).attr('data-lat')) + (Math.random() -.5) / 150000;
-    var lng = $(this).attr('data-lng');
+// $(function(){
+//    $('.updateContainer').click( function() {
+//     var marker;
+//     var marker_location;
+//     var lat = parseFloat($(this).attr('data-lat')) + (Math.random() -.5) / 150000;
+//     var lng = $(this).attr('data-lng');
     
-    var pos = new google.maps.LatLng(lat, lng);
-    marker = new google.maps.Marker({
-        position: pos,
-        map: map,
-        title: "Update Location"
-    });
-    map.setCenter(pos);
-    map.setZoom(16);
-   });
-});
+//     var pos = new google.maps.LatLng(lat, lng);
+//     marker = new google.maps.Marker({
+//         position: pos,
+//         map: map,
+//         title: "Update Location"
+//     });
+//     map.setCenter(pos);
+//     map.setZoom(16);
+//    });
+// });
--- app/assets/javascripts/updateMarker.js
+++ app/assets/javascripts/updateMarker.js
@@ -1,30 +1,33 @@
 // $(function(){
 	function addUpdateMarker(){
-	var toggle = 0;
+	toggle = 0;
+    console.log(toggle);
 
 	$('.updateContainer').click( function() {
 		var lat = $(this).attr('data-lat');
 	 	var lng = $(this).attr('data-lng');
 	 	var markerImg = $(this).attr('data-img');
-	 	
  		var pos = new google.maps.LatLng(lat, lng);
+        // console.log(toggle);
 
- 		if (toggle == 0){
- 			console.log(markerImg);
+ 		if (toggle == 0)
+        {
 		 	updateMarker = new google.maps.Marker({
-	        position: pos,
-	        map: map,
-	        title: "Update Location",
-	        icon: markerImg
-	      	});
-	      	toggle = 1;
-	      	map.setCenter(pos);
-	      	map.setZoom(16);
+	           position: pos,
+	           map: map,
+	           title: "Update Location",
+	           icon: markerImg
+	       });
+	       toggle = 1;
+	       map.setCenter(pos);
+	       map.setZoom(16);
 	 	}
-	 	else{
-	 		updateMarker.setMap(null);
+	 	else 
+	 	{
+            //console.log(toggle);
+            //updateMarker.setMap(null);	    
 	 		toggle = 0;
 	 	}
 	});
 	};
-// )};
+// });
